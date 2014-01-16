89aeb0fce1f8677e790b5a7652ce54cc7ec5f864
Farooq Yousuf <farooqyousuf1@gmail.com>
2014-01-16 14:29:04 -0500

    commiting mavtec code first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mavtec::Application.config.secret_key_base = '13db265ced5ded39991fbee8b7b74101f59ab70874195a37b74448a5fa372fe7c03e41d8ff19e0a65ba27914c77ffdff385078c02fc546fc509ef35a5dde98ae'
